5d18b70fbd5b3e19b4c0cd2bdaf0e937329d9f65
ndsanja <ndsanja@gmail.com>
2023-04-15 09:26:43 +0700

    fix next-config to build

---
+++ __next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  transpilePackages:["@mui/system", "@mui/material", "@mui/icons-material"],
+  modularizeImports: {
+    "@mui/material/?(((\\w*)?/?)*)": {
+      transform: "@mui/material/{{matches.[1]}}/{{member}}",
+    },
+    "@mui/icons-material/?(((\\w*)?/?)*)": {
+      transform: "@mui/icons-material/{{matches.[1]}}/{{member}}",
+    },
+  },
+};
+
+module.exports = nextConfig;
