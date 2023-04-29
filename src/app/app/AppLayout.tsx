4872d27e2a07c1dc737529b8c2d6675bb2d30d5a
ndsanja <ndsanja@gmail.com>
2023-04-30 03:18:58 +0700

    create organisasi elearing tab

--- src/app/app/AppLayout.tsx
+++ src/app/app/AppLayout.tsx
@@ -1,36 +1,8 @@
 'use client';
 
 import { Slide, useScrollTrigger } from '@mui/material';
-
-<<<<<<< HEAD
 import AppAppBar from './components/AppAppBar';
-
-type Props = {
-  children: React.ReactNode;
-};
-=======
-type Props = {
-  children: React.ReactNode;
-};
-
-interface PropsSlide {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
- window?: () => Window;
-  children: React.ReactNode;
-}
-
-function HideOnScroll(props: PropsSlide) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-  });
->>>>>>> 25a64896fd9ab8da26208b3c37f8f70403f661b1
+type Props = { children: React.ReactNode };
 
 // interface Props {
 //   /**
@@ -67,7 +39,6 @@ const AppLayoutClient = (props: Props) => {
       <div className="w-full max-w-[1400px] mx-auto pb-[68px]">
         {props.children}
       </div>
-
     </>
   );
 };
