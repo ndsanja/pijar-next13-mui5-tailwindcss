269a37ae2869e949d738c23ec1ce3ac1a7cb91fc
ndsanja <ndsanja@gmail.com>
2023-04-30 11:12:43 +0700

    add suspense

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from 'react';
 import AppLayoutClient from './AppLayout';
 import AppPageClient from './AppPage';
+import AppLoading from './loading';
 
 const AppPage = () => {
   return (
-    <AppLayoutClient>
-      <AppPageClient />
-    </AppLayoutClient>
+    <Suspense fallback={<AppLoading />}>
+      <AppLayoutClient>
+        <AppPageClient />
+      </AppLayoutClient>
+    </Suspense>
   );
 };
 export default AppPage;
